8311bee74c36b34e35bcd9e135ae99a0dba9ad2c
Robert <b00158877@mytudublin.ie>
2025-02-17 16:29:10 +0000

    folder structure created
    created frontend pages and began work on header and footer

---
+++ Frontend/Layouts/Header.php
@@ -0,0 +1,31 @@
+<?php
+<!DOCKTYPE html>
+<html lang="en">
+    <head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<nav class="Navbar navbar-expand-lg navbar-light bg-light">
+<div class="container">
+<a class="navbar-new" href=""</a>
+<button class="navbar-toggle" type="button" data-bs-toggle="collaspe" data-bs-target="navbarNAV"
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNAV"
+    <ul class="navbar-nav me-auto">
+<li class="nav-item"
+<a class="nav-link" href="Index.php">Home</a>
+</li>
+</ul>
+
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="login.html">Login</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="cart.html">Cart</a>
+</li>
