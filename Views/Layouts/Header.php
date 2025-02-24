d665dc6cf9d6a94ad98d537a623eea771a5e5469
Robert <b00158877@mytudublin.ie>
2025-02-24 16:26:49 +0000

    changes to header

--- Views/Layouts/Header.php
+++ Views/Layouts/Header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookStore</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Css linking -->
+        <link href="/Views/CSS/Style.css" rel="stylesheet"
 </head>
 
 <body>
@@ -12,25 +14,24 @@
 <div class="container">
     <!-- Logo section -->
     <a class="navbar-brand" href="../Index.php">
-        <img src="../Images/Logo.png" alt="Book store Logo" width="150"
+        <img src="/WebApps_assignment/Views/Images/Logo.png" alt="Book store Logo" width="150">
     </a>
 <a class="navbar-new" href=""></a>
-<button class="navbar-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNAV">
-    <span class="navbar-toggle-icon"></span>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNAV">
+    <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="navbarNAV">
     <ul class="navbar-nav me-auto">
 <li class="nav-item">
-<a class="nav-link" href="Index.php">Home</a>
+<a class="nav-link" href="../Index.php">Home</a>
 </li>
 </ul>
-
 <ul class="navbar-nav">
 <li class="nav-item me-3">
-<a class="nav-link" href="login.html">Login</a>
+<a class="nav-link" href="/Models/Login.php">Login</a>
 </li>
 <li class="nav-item me-3">
-<a class="nav-link" href="cart.html">Cart</a>
+<a class="nav-link" href="/Models/cart.php">Cart</a>
 </li>
 </ul>
 </div>
