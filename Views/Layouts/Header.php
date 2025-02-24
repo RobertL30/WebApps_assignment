15b5177eff80fd5707f5b700db29842f156e033a
Robert <b00158877@mytudublin.ie>
2025-02-24 17:59:57 +0000

    rewrote nav bar links and css
    reformatted nav bar html and css - added new items like books - members books etc..

--- Views/Layouts/Header.php
+++ Views/Layouts/Header.php
@@ -6,34 +6,43 @@
     <title>BookStore</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
         <!-- Css linking -->
-        <link href="/Views/CSS/Style.css" rel="stylesheet"
+        <link href="/Views/CSS/Style.css" rel="stylesheet">
 </head>
 
 <body>
 <nav class="Navbar navbar-expand-lg navbar-light bg-light">
 <div class="container">
     <!-- Logo section -->
-    <a class="navbar-brand" href="../Index.php">
+    <a class="navbar-brand" href="/Views/Index.php">
         <img src="/Views/Images/Logo.png" alt="Book store Logo" width="300">
     </a>
-<a class="navbar-new" href=""></a>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNAV">
-    <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNAV">
-    <ul class="navbar-nav me-auto">
-<li class="nav-item">
-<a class="nav-link" href="../Index.php">Home</a>
-</li>
-</ul>
-<ul class="navbar-nav">
-<li class="nav-item me-3">
-<a class="nav-link" href="/Models/Login.php">Login</a>
-</li>
-<li class="nav-item me-3">
-<a class="nav-link" href="/Models/cart.php">Cart</a>
-</li>
-</ul>
-</div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNAV">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNAV">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/Views/Index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Members Books</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/Models/Login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/Models/cart.php">Cart</a>
+            </li>
+        </ul>
+    </div>
 </div>
 </nav>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
