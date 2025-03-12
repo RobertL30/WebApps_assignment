8bfb8ceab6a89c4f2df6c27be3285f71cd902b20
Robert <b00158877@mytudublin.ie>
2025-03-12 17:32:34 +0000

    fixed some issues with styling and css linking on sign in page

--- Views/Index.php
+++ Views/Index.php
@@ -109,5 +109,6 @@
 
 
 <?php include 'Layouts\Footer.php' ; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- Views/Shop.php
+++ Views/Shop.php
@@ -327,5 +327,6 @@
 </div>
 
 <?php include 'Layouts/Footer.php'; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
