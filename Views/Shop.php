f21f59bf6700ae61ec234d592c8265d399c0d2e9
Robert <b00158877@mytudublin.ie>
2025-03-12 17:33:43 +0000

    fixed page names

--- Views/Shop.php
+++ Views/Shop.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop - BookStore</title>
+    <title>Paper Train - Store</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="/Views/CSS/Style.css" rel="stylesheet">
