e7b0c374b06374505ecbc93460c1184bd105e7df
Robert <b00158877@mytudublin.ie>
2025-03-12 15:25:10 +0000

    create shop.php page

---
+++ Views/Shop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop - BookStore</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="/Views/CSS/Style.css" rel="stylesheet">
+</head>
+<body>
+<?php include 'Layouts/Header.php'; ?>
+
+<-- Main content -->
+
+<?php include 'Layouts/Footer.php'; ?>
+</body>
+</html>
