da1d831817c63fc406f167c38d5681126a6f25d9
Robert <b00158877@mytudublin.ie>
2025-03-18 11:54:53 +0000

    Update sign_in_process.php
    gives user message to info of successful or unsuccessful sign in

--- Controllers/sign_in_process.php
+++ Controllers/sign_in_process.php
@@ -32,5 +32,38 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
             $_SESSION['username'] = $result['username'];
             $_SESSION['is_logged_in'] = true;
 
+            $login_success = true;
+        } else {
+            $login_error = true;
+
         }
     }
+//include the header to show successful sign in message
+
+include '../Views/Layouts/Header.php';
+?>
+
+<div class="container my-5"
+
+     <div class="col-md-6">
+         <?php if ($login_success): ?>
+         <div class="alert alert-success">
+             <h4> Your sign in was successful!</h4>
+             <p>Hello, <?php echo htmlspecialchars($_SESSION['username']);?> !</p>
+             <hr>
+             <p><a href="../Views/membersBooks.php" class="btn btn-primary">Browse members books</a></p>
+         </div>
+         <?php endif; ?>
+
+         <?php if ($login_error):?>
+
+         <div class="alert alert-danger">
+             <h4>Login unsuccessful.</h4>
+             <p>Please try again or register if you don't already have an account</p>
+             <hr>
+             <p><a href="../Views/auth/sign_in.php" class="btn btn-primary">Try again</a></p>
+             <hr>
+             <p><a href="../Views/auth/sign_up.php" class="btn btn-primary">Create an account</a></p>
+         </div>
+         <?php endif; ?>
+     </div>
