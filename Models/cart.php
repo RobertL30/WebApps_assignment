6440a150d8733afa0a4a72d5bd90452c41fd9607
Robert <b00158877@mytudublin.ie>
2025-03-18 17:22:10 +0000

    Update cart.php

--- Models/cart.php
+++ Models/cart.php
@@ -1,65 +1,60 @@
 <?php
+// Simple cart model based on sessions
 
-clss Cart {
-    //session based cart functions
+// We'll use simple functions rather than a class
+function addToCart($product_id, $title, $price, $quantity = 1) {
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = array();
+    }
 
-    //add the item to cart
-    public static function addToCart($product_id, $title, $price, $quantity = 1 ) {
-
-        if(!isset($_SESSION['cart'])) {
-            $_SESSION['cart'] = array();
-
-        }
-        //if item is in cart then increment quant.
-
-        if (isset(['cart']['$product_id'])) {
-            $_SESSION['cart']['$product_id']['quantity'] += $quantity;
-        } else {
-            //add new product to cart
-            $_SESSION['cart']['$product_id'] = array(
-                'title' =>? $title,
+    // If product already in cart, increase quantity
+    if (isset($_SESSION['cart'][$product_id])) {
+        $_SESSION['cart'][$product_id]['quantity'] += $quantity;
+    } else {
+        // Add new product to cart
+        $_SESSION['cart'][$product_id] = array(
+            'title' => $title,
             'price' => $price,
             'quantity' => $quantity
-            );
-        }
+        );
     }
+}
 
-    public static function updateCartItem($product_id, $quantity)
-    {
-        if (isset($_SESSION['cart']['$product_id'])) {
-            // if quant 0 or less remote item
-            if ($quantity <=0) {
-                self::removeCartItem($product_id);
-            } else {
-                $_SESSION['cart']['$product_id']['quantity'] = $quantity
-            }
-            return true;
+function updateCartItem($product_id, $quantity) {
+    if (isset($_SESSION['cart'][$product_id])) {
+        // If quantity is 0 or less, remove the item
+        if ($quantity <= 0) {
+            removeCartItem($product_id);
+        } else {
+            $_SESSION['cart'][$product_id]['quantity'] = $quantity;
         }
+        return true;
     }
-// Remove item from cart
-    public static function removeCartItem($product_id) {
-        if (isset($_SESSION['cart'][$product_id])) {
-            unset($_SESSION['cart'][$product_id]);
-            return true;
-        }
-        return false;
-    }
+    return false;
+}
 
-    // get cart content
-    public static function getCartContents() {
-        if (!isset($_SESSION['cart'])) {
-            $_SESSION['cart'] = array();
-        }
-        return $_SESSION['cart'];
+function removeCartItem($product_id) {
+    if (isset($_SESSION['cart'][$product_id])) {
+        unset($_SESSION['cart'][$product_id]);
+        return true;
     }
+    return false;
+}
 
-    // get cart total
-    public static function getCartTotal() {
-        $total = 0;
+function getCartContents() {
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = array();
+    }
+    return $_SESSION['cart'];
+}
+
+function getCartTotal() {
+    $total = 0;
+    if (isset($_SESSION['cart'])) {
         foreach ($_SESSION['cart'] as $item) {
             $total += $item['price'] * $item['quantity'];
         }
-        return $total;
     }
+    return $total;
 }
 ?>
