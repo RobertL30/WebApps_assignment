aec58ffae99db65ae5f8e9c171d60eca2b984f67
Robert <b00158877@mytudublin.ie>
2025-03-18 11:44:20 +0000

    syntax fixes for sign_in_process

--- Controllers/sign_in_process.php
+++ Controllers/sign_in_process.php
@@ -1,36 +1,36 @@
 <?php
 
-require_once '../Config/DBconnect.php'
+require_once '../Config/DBconnect.php';
+require_once '../common.php';
 
 //kick off the session
-start_session();
+session_start();
 
-$login_error = flase;
+$login_error = false;
 
-if (isset($_POST['username']) && isset($_POST['password']))
+if (isset($_POST['username']) && isset($_POST['password'])) {
     //preps SQL statement to check the user details
-    $sql = "SELECT from WHERE username =: username AND password =: password";
+    $sql = "SELECT * from users WHERE username = :username AND password = :password";
 
         //will sanitise inputs
-        $username = escape($_POST['username']);
-        $password = escape($_POST['password']);
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
         $statement = $connection->prepare($sql);
-        $statement->bindParam(':username', $username, PDO::PARAM_STR)
-        $statement->bindParam(':password', $password, PDO::PARAM_STR)
-            $statemen->execute();
+        $statement->bindParam(':username', $username, PDO::PARAM_STR);
+        $statement->bindParam(':password', $password, PDO::PARAM_STR);
+            $statement->execute();
 
         $result = $statement->fetch(PDO::FETCH_ASSOC);
 
         if ($statement->rowCount() > 0 ) {
-            //start session
-            session_start();
+
 
             //store user info for session
 
-            $_SESSION['user_id'] = $result['ID'];
+            $_SESSION['user_id'] = $result['id'];
             $_SESSION['username'] = $result['username'];
             $_SESSION['is_logged_in'] = true;
 
         }
-
+    }
