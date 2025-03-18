5a89261f56fef903a35d692de3820e362b76867c
Robert <b00158877@mytudublin.ie>
2025-03-18 17:25:29 +0000

    Update CartController.php
    syntax fixed

--- Controllers/CartController.php
+++ Controllers/CartController.php
@@ -5,16 +5,16 @@ require_once '../common.php';
 
 // Start session
 if (session_status() == PHP_SESSION_NONE) {
-    session_start()
+    session_start();
 }
 
 // Process add to cart action
 if (isset($_POST['add_to_cart'])) {
     $product_id = $_POST['product_id'];
     $title = $_POST['title'];
-    $price = $_POST['prie'];
+    $price = $_POST['price'];
 
-    cart::addToCart($product_id, $title, $price);
+    Cart::addToCart($product_id, $title, $price);
 
     // Redirect to shop page
     header("Location: /Views/Shop.php");
@@ -23,10 +23,10 @@ if (isset($_POST['add_to_cart'])) {
 
 // Process update cart action
 if (isset($_POST['update_cart'])) {
-    $product_id = $_POST['product_id']
-    $quantity = $_POST['quantity']
+    $product_id = $_POST['product_id'];
+    $quantity = $_POST['quantity'];
 
-    cart::updateCartItem($product_id, $quantity);
+    Cart::updateCartItem($product_id, $quantity);
 
     // Redirect back to cart page
     header("Location: /Views/cart.php");
@@ -37,7 +37,7 @@ if (isset($_POST['update_cart'])) {
 if (isset($_POST['remove_from_cart'])) {
     $product_id = $_POST['product_id'];
 
-    cart::removeCartItem($product_id);
+    Cart::removeCartItem($product_id);
 
     // Redirect back to cart page
     header("Location: /Views/cart.php");
