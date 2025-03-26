a32b67207f414edec170197c6e8441be9753f873
Robert <b00158877@mytudublin.ie>
2025-03-26 10:47:58 +0000

    update cart to use class structure

--- Models/cart.php
+++ Models/cart.php
@@ -1,29 +1,27 @@
 <?php
-// Simple cart model based on sessions
-
-// We'll use simple functions rather than a class
-function addToCart($product_id, $title, $price, $quantity = 1) {
-    if (!isset($_SESSION['cart'])) {
-        $_SESSION['cart'] = array();
-    }
 
 
-    // if product already in cart increase quant
-    if (isset($_SESSION['cart'][$product_id])) {
-        $_SESSION['cart'][$product_id]['quantity'] += $quantity;
-    } else {
-        // Add new product to cart
-        $_SESSION['cart'][$product_id] = array(
-            'title' => $title,
-            'price' => $price,
-            'quantity' => $quantity
-        );
+class Cart {
+    public static function addToCart($product_id, $title, $price, $quantity = 1) {
+        if (!isset($_SESSION['cart'])) {
+            $_SESSION['cart'] = array();
+        }
+        // if product already in cart increase quant
+        if (isset($_SESSION['cart'][$product_id])) {
+            $_SESSION['cart'][$product_id]['quantity'] += $quantity;
+        } else {
+            // Add new product to cart
+            $_SESSION['cart'][$product_id] = array(
+                'title' => $title,
+                'price' => $price,
+                'quantity' => $quantity
+            );
+        }
     }
 }
 
-function updateCartItem($product_id, $quantity) {
-    if (isset($_SESSION['cart'][$product_id])) {
-        // If quant is 0 or less remove the item
+public static function updateCartItem($product_id, $quantity) {
+    if (isset($_SESSION['cart']['$product_id'])) {
         if ($quantity <= 0) {
             removeCartItem($product_id);
         } else {
@@ -34,22 +32,36 @@ function updateCartItem($product_id, $quantity) {
     return false;
 }
 
-function removeCartItem($product_id) {
+public static function updateCartItem($product_id, $quantity) {
+    if (isset($_SESSION['cart'][$product_id])) {
+        // If quant is 0 or less remove the item
+        if ($quantity <= 0) {
+            self::removeCartItem($product_id);
+        } else {
+            $_SESSION['cart'][$product_id]['quantity'] = $quantity
+        }
+        return true;
+        }
+    return false;
+    }
+
+//remove item from cart
+public static function removeCartItem($product_id) {
     if (isset($_SESSION['cart'][$product_id])) {
         unset($_SESSION['cart'][$product_id]);
         return true;
     }
     return false;
 }
-
-function getCartContents() {
+//get cart contents
+public static function getCartContents() {
     if (!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = array();
     }
     return $_SESSION['cart'];
 }
 
-function getCartTotal() {
+public static function getCartTotal() {
     $total = 0;
     if (isset($_SESSION['cart'])) {
         foreach ($_SESSION['cart'] as $item) {
