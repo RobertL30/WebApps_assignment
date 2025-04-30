10184c97e70c02a59129cb87196fc3c18fd2e5aa
Robert <b00158877@mytudublin.ie>
2025-04-30 10:47:12 +0100

    Update DBconnect.php

--- Config/DBconnect.php
+++ Config/DBconnect.php
@@ -1,3 +1,11 @@
 <?php
 
-require_once 
+require_once '../Config.php'; // accesses the credentials
+
+try [
+    $connection = new PDO($dsn,$username,$password,$options);
+} catch (/PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+
+}
+?>
