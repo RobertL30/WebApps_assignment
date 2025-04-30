2443f44fd529d4ef04a202ace848379631022407
Robert <b00158877@mytudublin.ie>
2025-04-30 10:52:14 +0100

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,8 +1,11 @@
 <?php
-
 $host = "localhost";
 $dbname = "webapp_db";
 $username ="root";
 $password = "";
+$dsn ="mysql:host=$host;dbname=$dbname";
+$options = array(
+    PDO:ATT_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
 ?>
 
